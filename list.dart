7ab8034bfa2e18c75420bfd0d2cefab1ad4efc40
Fauzan Falah <fauzancodekop@gmail.com>
2022-05-31 16:06:55 +0700

    update

---
+++ list.dart
@@ -0,0 +1,7 @@
+void main() {
+  //Spread Operator
+  var favorites = ['Seafood', 'Salad', 'Nugget', 'Soup'];
+  var others = ['Cake', 'Pie', 'Donut'];
+  var allFavorites = [...favorites, ...others];
+  print(allFavorites);
+}
---
+++ set.dart
@@ -0,0 +1,5 @@
+void main() {
+  var numberSet = {1, 4, 6};
+  Set<int> anotherSet = new Set.from([1, 4, 6, 4, 1]);
+  print(anotherSet);
+}
